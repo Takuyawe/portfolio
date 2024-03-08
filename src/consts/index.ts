c339088357fcef7a0de438f446742eb8cccc1ff9
Takuyawe <takuslugger200@gmail.com>
2024-03-08 16:04:12 +0900

    feature: create projects

---
+++ src/consts/index.ts
@@ -0,0 +1,58 @@
+export const MY_SKILLS = [
+    {
+        name: "HTML",
+        icon: "skills/html5"
+    },
+    {
+        name: "CSS",
+        icon: "skills/css"
+    },
+    {
+        name: "JavaScript",
+        icon: "skills/js"
+    },
+    {
+        name: "TypeScript",
+        icon: "skills/typescript"
+    },
+    {
+        name: "React",
+        icon: "skills/reactjs"
+    },
+    {
+        name: "Next",
+        icon: "skills/nextjs"
+    },
+    {
+        name: "Node",
+        icon: "skills/nodejs"
+    },
+    {
+        name: "AWS",
+        icon: "skills/aws"
+    },
+    {
+        name: "Git",
+        icon: "skills/git"
+    },
+    {
+        name: "GitLab",
+        icon: "skills/gitlab"
+    },
+    {
+        name: "Postgres",
+        icon: "skills/postgresql"
+    },
+    {
+        name: "Linux",
+        icon: "skills/linux"
+    },
+    {
+        name: "Python",
+        icon: "skills/python"
+    },
+    {
+        name: "Docker",
+        icon: "skills/docker"
+    }
+]
