565318034344fce44d3dcd3772c3e5b5b17a437b
Takuyawe <takuslugger200@gmail.com>
2024-03-08 14:35:52 +0900

    feature: apply key events

---
+++ src/modules/index.ts
@@ -0,0 +1,16 @@
+export const getViewSectionId = (pageId: number) => {
+    switch (pageId) {
+        case 0:
+            return "home"
+        case 1:
+            return "about"
+        case 2:
+            return "skills"
+        case 3:
+            return "projects"
+        case 4:
+            return "contact"
+        default:
+            return "home"
+    }
+}
