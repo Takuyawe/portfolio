61c57291273ae364c49959f52014ec1dfc242b8d
Takuyawe <takuslugger200@gmail.com>
2024-08-25 22:14:47 +0900

    config: add ts parser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import parserTypescript from "@typescript-eslint/parser";
 import pluginAstro from "eslint-plugin-astro";
 import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
