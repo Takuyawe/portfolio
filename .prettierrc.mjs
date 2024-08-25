f7c27b39713d51b0cc0a4a4e8a931c912f95c640
Takuyawe <takuslugger200@gmail.com>
2024-08-25 21:25:14 +0900

    config: add astro option for formatting

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -11,4 +11,12 @@ export default {
   semi: false,
   singleQuote: true,
   plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-astro', "eslint-config-prettier"],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
