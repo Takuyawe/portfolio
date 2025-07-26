63ab11366cfe8ba08b527c64b39b63372d88cea8
Takuyawe <takuslugger200@gmail.com>
2025-07-26 16:46:38 +0900

    config: correctly configure astro parser

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,33 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import parserTypescript from "@typescript-eslint/parser";
-import pluginAstro from "eslint-plugin-astro";
-import eslintConfigPrettier from "eslint-config-prettier";
-import globals from "globals";
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import parserTypescript from '@typescript-eslint/parser'
+import pluginAstro from 'eslint-plugin-astro'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginAstro.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs,ts,astro}"],
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       parser: parserTypescript,
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json'
       },
-      globals: globals.browser,
-    },
+      globals: globals.browser
+    }
   },
-  eslintConfigPrettier,
-];
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parser: pluginAstro.parser,
+      parserOptions: {
+        parser: parserTypescript,
+        project: './tsconfig.json'
+      }
+    }
+  },
+  eslintConfigPrettier
+]
